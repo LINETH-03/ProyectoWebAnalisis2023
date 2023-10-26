361875c151e13093beb59e9d2cee9324096ff90a
LINETH-03 <lperezh9@miumg.edu.gt>
2023-10-26 02:51:12 -0600

    pruebas de frontend

---
+++ frontend/src/Pages/Layout.jsx
@@ -0,0 +1,17 @@
+import { NavLink, Outlet } from "react-router-dom"
+
+export const Layout =()=>{
+
+    return(
+        <div>
+            <NavLink to="/Users">
+                Usuarios
+            </NavLink>
+            
+            <NavLink to="/Teachers">
+                Profesores
+            </NavLink>
+            <Outlet/>
+        </div>
+    )
+}
