052fa019abbc5ce761130fbe5a672922669fe7ec
LINETH-03 <lperezh9@miumg.edu.gt>
2023-10-26 21:21:26 -0600

    api final

--- api/app.js
+++ api/app.js
@@ -8,6 +8,8 @@ const cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var informeRouter = require('./routes/informe');
+var ingresoRouter = require('./routes/ingreso');
+var retirosRouter = require('./routes/retiros');
 
 var app = express();
 
@@ -27,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/informe', informeRouter);
+app.use('/ingreso', ingresoRouter);
+app.use('/retiros', retirosRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
